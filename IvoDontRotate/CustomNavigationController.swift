c70ea820cd215f055856e962541bc8869df09c3d
Devxhkl <devxhkl@outlook.com>
2015-12-08 00:14:56 -0500

    Added comments

--- IvoDontRotate/CustomNavigationController.swift
+++ IvoDontRotate/CustomNavigationController.swift
@@ -12,10 +12,16 @@ class CustomNavigationController: UINavigationController {
   
   override func shouldAutorotate() -> Bool {
     if !viewControllers.isEmpty {
+      
+      // Check if this ViewController is the one you want to disable roration on
       if topViewController!.isKindOfClass(ViewController) {
+        
+        // If true return false to disable it
         return false
       }
     }
+    
+    // Else normal rotation enabled
     return true
   }
 }
